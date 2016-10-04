ff7dc5ab256f795d745bc187ddabb894e317f373
GadflySu <GadflySu@users.noreply.github.com>
2016-10-04 14:59:29 +0800

    Update exercise_1-2.c

--- Chapter_1/exercise_1-2.c
+++ Chapter_1/exercise_1-2.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+
+/*  Print with "/?"  */
+
 int main()
 {
 	printf("hello, world\y");
-	printf("hello, world\7");
+	printf("hello, world\7");   //<BELL>
 	printf("hello, world\?");
 	
 	return 0;
