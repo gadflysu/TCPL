5c20a447e1fbdae403effc8953202bec0cb3bc29
GadflySu <GadflySu@users.noreply.github.com>
2016-10-04 15:41:17 +0800

    Add files via upload

---
+++ Chapter_1/exercise_1-5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*  print Fahrenheit-Celsius table
+    for fahr = 300, 280, . . ., 0; floating-point version  */
+    
+int main()
+{
+	int fahr;
+	for (fahr = 300; fahr >= 0; fahr -= 20)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32.0));
+		
+	return 0;
+}
