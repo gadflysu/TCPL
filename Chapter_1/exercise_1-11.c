870293aad4ec3fa9799b84b06bdf1697f3ac24c1
GadflySu <GadflySu@users.noreply.github.com>
2016-10-04 21:09:37 +0800

    Add files via upload

---
+++ Chapter_1/exercise_1-11.c
@@ -0,0 +1,14 @@
+
+/* How to test the word count program: (maybe)
+ *  1. no input;
+ *  2. no words, only newlines;
+ *  3. no words, only blanks, tabs, and newlines;
+ *  4. one word per line (no blanks and tabs);
+ *  5. the word appears at the beginning of the line;
+ *  6. the word appears after a string of blanks;
+ */
+
+int main()
+{
+	return 0;
+}
