042b09d60482f3e148a481470a550376d3a34abc
GadflySu <GadflySu@users.noreply.github.com>
2016-10-04 15:16:53 +0800

    Update exercise_1-3.c

--- Chapter_1/exercise_1-3.c
+++ Chapter_1/exercise_1-3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*  print Fahrenheit-Celsius table
-		for fahr = 0, 20, . . ., 300; floating-point version  */
+    for fahr = 0, 20, . . ., 300; floating-point version  */
 		
 int main()
 {
