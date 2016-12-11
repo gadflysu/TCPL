5d8d1d6ce6c019a959b7205c40749d8c1a869ea3
GadflySu <GadflySu@users.noreply.github.com>
2016-12-11 15:55:48 +0800

    Update exercise_1-14.c

--- Chapter_1/exercise_1-14.c
+++ Chapter_1/exercise_1-14.c
@@ -5,44 +5,32 @@
 
 int main()
 {
-	int nc[MAXCHAR] = {0};
-	int i, len, maxnc;
-	int c;
-	
+	int c, i;
+	int len;		/*  length of each bar		*/
+	int maxvalue;           /*  maximum value of nc[]	*/
+	int nc[MAXCHAR] = {0};  /*  character counters		*/
+
 	while ((c = getchar()) != EOF) {
-		++nc[c];
+		if (c < MAXCHAR)
+			++nc[c];
 	}
-	maxnc = 0;
-	for (i = 0; i < MAXCHAR; i++)
-		if (nc[i] > maxnc)
-			maxnc = nc[i];
-	for (i = 0; i < MAXCHAR; i++) {
-		if (nc[i] > 0) {
-			if (i != ' ' && i != '\n' && i != '\t') {
-				printf(" ");
-				putchar(i);
-			}
-                        else
-				switch (i) {
-					case ' ' :
-						printf("\\_");
-						break;
-					case '\n' :
-						printf("\\n");
-						break;
-					case '\t' :
-						printf("\\t");
-						break;
-				}
-			printf(" - %3d :  ", nc[i]);
-			if ((len = nc[i]/maxnc*MAXHIST) <= 0)
+	maxvalue = 0;
+	for (i = 1; i < MAXCHAR; i++)
+		if (nc[i] > maxvalue)
+			maxvalue = nc[i];
+	for (i = 1; i < MAXCHAR; i++) {
+		if (isprint(i))
+			printf("%5d - %c - %5d :  ", i, i, nc[i]);
+		else
+			printf("%5d -   - %5d :  ", i, nc[i]);
+                if (nc[i] > 0) {
+			if ((len = nc[i]/maxvalue*MAXHIST) <= 0)
 				len = 1;
-//			else
-//				len = 0;
-			while (len-- > 0)
-				printf("*");
-			printf("\n");
-		}
+		} else
+			len = 0;
+		while (len-- > 0)
+			printf("*");
+		putchar('\n');
 	}
 	
 	return 0;
