8200f14c11e73b3e09b40532aa672339eda237a9
GadflySu <GadflySu@users.noreply.github.com>
2016-12-11 14:05:15 +0800

    Update exercise_1-13.c

--- Chapter_1/exercise_1-13.c
+++ Chapter_1/exercise_1-13.c
@@ -1,53 +1,57 @@
 #include <stdio.h>
 
-#define LIM 10000
-#define IN  1
-#define OUT 0
+#define MAXHIST 15	/*  max length of histogram	*/
+#define MAXWORD 11	/*  max length of a word	*/
+#define IN  	1	/*  inside a word		*/
+#define OUT 	0	/*  outside a word		*/
 
-void showChart(int chart[], int *maxL);
-
+/*  print horizontal histogram				*/
 int main()
 {
-        int i, j;
-        int c;
-        int lenWord, maxLen, state;
-        int chart[LIM] = {0};
+        int c, i, nc, state;
+        int len;                        /*  length of each bar		*/
+        int maxvalue;                   /*  maximum value for wl[]  	*/
+        int overflow;                   /*  number of overflow words  	*/
+        int wl[MAXWORD] = {0};          /*  word length counters	*/
         
 	state = OUT;
-	lenWord = 0;
-	maxLen = lenWord;
+	nc = 0;                         /*  number of chars in a word	*/
+	overflow = 0;                   /*  number of words >= MAXWORD	*/
 	while ((c = getchar()) != EOF) {
 		if (c == ' ' || c == '\n' || c == '\t') {        	 /* finish the word  */
-			if (state == IN) {                              
-				state = OUT;
-				++chart[lenWord];
-				maxLen = (maxLen > lenWord) ? maxLen : lenWord;
-				lenWord = 0;
-			}
-		} else {						/*  beginning of word  */
-			++lenWord;
-			if (state == OUT)		
+			state = OUT;
+			if (nc > 0)
+				if (nc < MAXWORD)
+					++wl[nc];
+				else
+					++overflow;
+			nc = 0;
+		} else {						
+			++nc;
+			if (state == OUT)		                /*  beginning of word  */
 				state = IN;     
 		}
 	}
-	++chart[lenWord];
-	
-	showChart(chart, &maxLen);
+	maxvalue = 0;
+	for (i = 1; i < MAXWORD; ++i)
+		if (wl[i] > maxvalue)
+			maxvalue = wl[i];
+	for (i = 1; i < MAXWORD; ++i) {
+		printf("%5d - %5d : ", i, wl[i]);
+		if (wl[i] > 0) {
+			if ((len = wl[i]*MAXHIST/maxvalue) <= 0)
+				len = 1;
+		} else
+			len = 0;
+		while (len > 0) {
+			putchar('*');
+			--len;
+		}
+		putchar('\n');
+	}
+	if (overflow > 0)
+		printf("There are %d words >= %d\n", overflow, MAXWORD);
 
 	return 0;
-}
-
-void showChart(int chart[], int *maxL)
-{
-	int i, j;
-	int maxLen = *maxL;
-	
-	for (i = 1; i <= maxLen; i++) {
-		printf("%d | ", i);
-		for (j = 0; j < chart[i]; j++) {
-			printf("*");
-		}
-		printf("\n");
-	}
 }
 
