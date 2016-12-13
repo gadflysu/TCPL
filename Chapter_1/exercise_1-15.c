8f6689a93ea1fa9f0362b9ff52a40143427b5c1f
GadflySu <GadflySu@users.noreply.github.com>
2016-12-13 15:06:41 +0800

    Update exercise_1-15.c

--- Chapter_1/exercise_1-15.c
+++ Chapter_1/exercise_1-15.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 
-double f2c(double f);
+double celsius(double fahr);
 
+/*  print Celsius-Fahrenheit table
+    for celsius = 0, 20, . . ., 300; floating-point version  */
 int main()
 {
-	double fahr, celsius;
+	double fahr;
+	int lower, upper, step;
 	
-	scanf("%lf", &fahr);
-	printf("%.2lf\n", f2c(fahr));
+	lower = 0;    /*  lower lmit of temperature table  */
+	upper = 300;  /*  upper limit                      */
+	step = 20;    /*  step size                        */
+
+	fahr = lower;
+	while (fahr <= upper) {
+		printf("%3.0f %6.1f\n", fahr, celsius(fahr));
+		fahr += step;
+	}
 
 	return 0;
 }
 
-double f2c(double f)
+/*  celsius: convert fahr into celsius	*/
+double celsius(double f)
 {
 	return (5.0/9.0)*(f-32.0);
 }
