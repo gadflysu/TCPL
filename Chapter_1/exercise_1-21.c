01cee12f91edc1d56c1738cfeda7503e017e44ec
GadflySu <GadflySu@users.noreply.github.com>
2016-12-20 18:45:26 +0800

    Update exercise_1-21.c

--- Chapter_1/exercise_1-21.c
+++ Chapter_1/exercise_1-21.c
@@ -6,6 +6,41 @@ void rmBlanks(char bf[], int *pos);
 void clearBf(char a[], int *pos);
 
 /*  erplace blanks with the least number of tabs and blanks	*/
+/*  solution from The C Answer Book				*/
+/*
+int main()
+{
+	int c, nb, nt, pos;
+
+	nb = 0;                 			//  # of blanks necessary
+	nt = 0;                 			//  # of tabs necessary
+	for (pos = 1; (c = getchar()) != EOF; ++pos) {
+		if (c == ' ') {
+			if (pos % TABINC != 0)
+				++nb;           	//  increment # of blanks
+			else {
+				nb = 0;         	//  reset # of blanks
+				++nt;           	//  one more tab
+			}
+		} else {
+			for ( ; nt > 0; --nt)
+				putchar('\t');  	//  output tab(s)
+			if (c == '\t')          	//  forget the blank(s)
+				nb = 0;
+			else                    	//  output blank(s)
+				for ( ; nb > 0; --nb)
+					putchar(' ');
+			putchar(c);
+			if (c == '\n')
+				pos = 0;
+			else if (c == '\t')
+				pos = pos + (TABINC - pos%TABINC);
+		}
+	}
+}
+*/
+
+/*  another solution by GadflySu			*/
 int main()
 {
         int c, nb, pos;
